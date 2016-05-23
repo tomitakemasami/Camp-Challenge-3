49ab72acb6a5daae4c69953fdfb75ab0db7b34d1
冨武真生 <genuinelive1124@gmail.com>
2016-05-23 16:43:54 +0900

    JavaからのDBアクセス　課題１

---
+++ src/java/JDBC/jdbc1.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JDBC;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ *
+ * @author tomitakemasami
+ */
+
+//課題1:Challenge_dbへのエラーハンドリングを含んだ接続の確立を行ってください
+
+
+
+public class jdbc1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        Connection con = null;
+        
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://localhost:8889/Challenge_db","root","root");
+            
+            out.println("MySQLに接続できました。");
+            
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            out.println("JDBCドライバのロードに失敗しました。");
+                    
+        } catch (SQLException e) {
+            out.println("MySQLに接続できませんでした。");
+            
+            
+            
+//preparedStatement a = con.prepareStstement("create table user(userID int, name varchar(255),age int)");
+            
+            
+            
+            
+            
+        
+        }finally{
+            if(con != null){
+                try{
+                    con.close();
+                }catch(SQLException e){
+                    System.out.println("MySQLのクローズに失敗しました。");
+                    
+                }
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
